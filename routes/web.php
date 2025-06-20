0664d5c201dabcb49bf42105461fcc070f6cd34d
Mahmoud El Absi <mahmoudalabsi0599@gmail.com>
2025-06-20 18:20:19 +0300

    ready for render deployment

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DocumentController;
 
+Route::get('/', [DocumentController::class, 'index'])->name('documents.index');
 Route::get('/stats', [DocumentController::class, 'stats'])->name('documents.stats');
 Route::prefix('documents')->group(function () {
-    Route::get('/', [DocumentController::class, 'index'])->name('documents.index');
     Route::post('/upload', [DocumentController::class, 'upload'])->name('documents.upload');
 
     Route::get('/search', [DocumentController::class, 'search'])->name('documents.search');
